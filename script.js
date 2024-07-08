a7656d2c2c003fdd62ff86728aa4e58342dc6ed8
Xethyr <monteiv@me.com>
2024-07-08 00:01:41 -0500

    Placeholder box text removed. Functionality now supports finding solution on any guess.

--- script.js
+++ script.js
@@ -34,23 +34,71 @@ let guesses = 0;
 
 const solution = 'saute';
 
+function winGame() {
+    if (guesses === 1) {
+        box1.classList.add('correct');
+        box2.classList.add('correct');
+        box3.classList.add('correct');
+        box4.classList.add('correct');
+        box5.classList.add('correct');
+        document.getElementById('guess-input').value = '';
+        return;
+    }
+    if (guesses === 2) {
+        box6.classList.add('correct');
+        box7.classList.add('correct');
+        box8.classList.add('correct');
+        box9.classList.add('correct');
+        box10.classList.add('correct');
+        document.getElementById('guess-input').value = '';
+        return;
+    }
+    if (guesses === 3) {
+        box11.classList.add('correct');
+        box12.classList.add('correct');
+        box13.classList.add('correct');
+        box14.classList.add('correct');
+        box15.classList.add('correct');
+        document.getElementById('guess-input').value = '';
+        return;
+    }
+    if (guesses === 4) {
+        box16.classList.add('correct');
+        box17.classList.add('correct');
+        box18.classList.add('correct');
+        box19.classList.add('correct');
+        box20.classList.add('correct');
+        document.getElementById('guess-input').value = '';
+        return;
+    }
+    if (guesses === 5) {
+        box21.classList.add('correct');
+        box22.classList.add('correct');
+        box23.classList.add('correct');
+        box24.classList.add('correct');
+        box25.classList.add('correct');
+        document.getElementById('guess-input').value = '';
+        return;
+    }
+    if (guesses === 6) {
+        box26.classList.add('correct');
+        box27.classList.add('correct');
+        box28.classList.add('correct');
+        box29.classList.add('correct');
+        box30.classList.add('correct');
+        document.getElementById('guess-input').value = '';
+        return;
+    }
+
+}
+
 function checkGuess() {
     const guessInput = document.getElementById('guess-input');
     const guessArray = guessInput.value.split('');
     const solutionArray = solution.split('');
-    console.log(guessInput.value);
-    console.log(guesses);
 
     if (guessInput.value === solution) {
-        if (guesses === 1) {
-            box1.classList.add('correct');
-            box2.classList.add('correct');
-            box3.classList.add('correct');
-            box4.classList.add('correct');
-            box5.classList.add('correct');
-            document.getElementById('guess-input').value = '';
-            return;
-        }
+        winGame();
     }
     // for (let i = 0; i < guessArray.length; i++) {
     
@@ -68,6 +116,51 @@ function appendGuess() {
         box3.innerText = guessArray[2];
         box4.innerText = guessArray[3];
         box5.innerText = guessArray[4];
+        checkGuess();
+        return;
+    }
+    if (guesses === 2) {
+        box6.innerText = guessArray[0];
+        box7.innerText = guessArray[1];
+        box8.innerText = guessArray[2];
+        box9.innerText = guessArray[3];
+        box10.innerText = guessArray[4];
+        checkGuess();
+        return;
+    }
+    if (guesses === 3) {
+        box11.innerText = guessArray[0];
+        box12.innerText = guessArray[1];
+        box13.innerText = guessArray[2];
+        box14.innerText = guessArray[3];
+        box15.innerText = guessArray[4];
+        checkGuess();
+        return;
+    }
+    if (guesses === 4) {
+        box16.innerText = guessArray[0];
+        box17.innerText = guessArray[1];
+        box18.innerText = guessArray[2];
+        box19.innerText = guessArray[3];
+        box20.innerText = guessArray[4];
+        checkGuess();
+        return;
+    }
+    if (guesses === 5) {
+        box21.innerText = guessArray[0];
+        box22.innerText = guessArray[1];
+        box23.innerText = guessArray[2];
+        box24.innerText = guessArray[3];
+        box25.innerText = guessArray[4];
+        checkGuess();
+        return;
+    }
+    if (guesses === 6) {
+        box26.innerText = guessArray[0];
+        box27.innerText = guessArray[1];
+        box28.innerText = guessArray[2];
+        box29.innerText = guessArray[3];
+        box30.innerText = guessArray[4];
         checkGuess();
         return;
     }
